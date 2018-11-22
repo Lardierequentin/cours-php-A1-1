8aadfec63fa6dc39f3a4339326eb43d29cfae57f
traderbook <contact@traderbook.net>
2018-11-22 16:50:45 +0100

    Je fais le job du DIGI

--- todo/index.php
+++ todo/index.php
@@ -51,7 +51,7 @@ if(!empty($_POST["task"])) {
 
 if(isset($_GET["update"])) {
     $index = $_GET["update"];
-    $_SESSION["tasks"][$index]["status"] = false;
+    $_SESSION["tasks"][$index]["status"] = !$_SESSION["tasks"][$index]["status"];
 
     redirection();
 }
@@ -68,45 +68,57 @@ if(isset($_GET["delete"])) {
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 </head>
 
-<?php include 'layout/header.php'?>
+<div class="container">
+    <?php include 'layout/header.php'?>
 
-<form action="" method="post">
-    <input type="text" name="task" placeholder="Saisir une tâche...">
-    <button type="submit">OK</button>
-    <a href="?clean=1">Clean</a>
-</form>
-<table>
-    <thead>
-    <tr>
-        <th>#</th>
-        <th>Nom de la tâche</th>
-        <th>Checker</th>
-        <th>Modifier</th>
-        <th>Supprimer</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php if(count($_SESSION["tasks"])): ?>
-    <?php foreach ($_SESSION["tasks"] as $index => $task): ?>
+    <form class="form-inline" action="" method="post">
+        <input class="form-control mb-2 mr-sm-2" type="text" name="task" placeholder="Saisir une tâche...">
+        <button type="submit" class="btn btn-primary mb-2">OK</button>
+        <a href="?clean=1" class="btn btn-danger mb-2">Clean</a>
+    </form>
+    <table class="table">
+        <thead>
         <tr>
-            <!-- Ceci est presque comme une condition. C'est un ternaire -->
-            <td><?php echo $task["status"] ? "+" : "-" ?></td>
-            <td><?php echo $task["task"] ?></td>
-            <td><a href="?update=<?php echo $index ?>">check</a></td>
-            <td><a href="update.php?index=<?php echo $index ?>">modifier</a></td>
-            <td><a href="?delete=<?php echo $index ?>">supprimer</a></td>
+            <th>#</th>
+            <th>Nom de la tâche</th>
+            <th>Checker</th>
+            <th>Modifier</th>
+            <th>Supprimer</th>
         </tr>
-    <?php endforeach; ?>
-    <?php else: ?>
-        <tr>
-            <td colspan="5">Aucune tâche, veuillez en saisir une...</td>
-        </tr>
-    <?php endif ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+        <?php if(count($_SESSION["tasks"])): ?>
+            <?php foreach ($_SESSION["tasks"] as $index => $task): ?>
+                <tr>
+                    <!-- Ceci est presque comme une condition. C'est un ternaire -->
+<!--                    <td>--><?php //echo $task["status"] ? "+" : "-" ?><!--</td>-->
+                    <td>
+                        <?php if($task["status"]): ?>
+                            <i class="fas fa-times"></i>
+                        <?php else: ?>
+                            <i class="fas fa-check"></i>
+                        <?php endif ?>
+                    </td>
 
-<?php include 'layout/footer.php'; ?>
+                    <td><?php echo $task["task"] ?></td>
+                    <td><a class="btn btn-secondary" href="?update=<?php echo $index ?>">check</a></td>
+                    <td><a class="btn btn-info" href="update.php?index=<?php echo $index ?>">modifier</a></td>
+                    <td><a class="btn btn-danger" href="?delete=<?php echo $index ?>">supprimer</a></td>
+                </tr>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <tr>
+                <td colspan="5">Aucune tâche, veuillez en saisir une...</td>
+            </tr>
+        <?php endif ?>
+        </tbody>
+    </table>
 
+    <?php include 'layout/footer.php'; ?>
+</div>
 </html>
